2cfd11d557dd0271376202ce15624e0c0e04aab3
jim leach <yogajim@gmail.com>
2015-05-24 19:16:39 -0400

    Style main content and starting on color buttons

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,17 @@
 
 
 $(document).ready( function () {
-  var artwork, title, price, seller, everything;
+  var artwork, title, price, seller, eicon, eiconb, everything;
 
   elvis.results.forEach(function (items){
      title = '<p class="etitle">' + items.title + '</p>';
-     artwork = '<img src= "'+ items.Images[0].url_170x135 +'" />';
-     price = '<p class="eprice">' + "$" + items.price + "USF" + '</p>';
+     artwork = '<img class="epic" src= "'+ items.Images[0].url_170x135 +'" />';
+     price = '<p class="eprice">' + "$" + items.price + " USD" + '</p>';
      seller = '<p class="eseller">' + items.Shop.shop_name + '</p>';
-     everything = '<div class="everything">' + artwork + title + seller + price + '</div>';
+     eicon = '<span class="epic1"><img src="images/hamburger.png"></span>';
+     eiconb = '<span class="epic2"><img src="images/heart.png"></span>';
+
+     everything = '<div class="everything">' + artwork + title + seller + price + eicon + eiconb + '</div>';
 
          $('.maine').append(everything);
 
