388b97c00fcb07c039572d334eac98e248279e72
jim leach <yogajim@gmail.com>
2015-05-23 21:09:12 -0400

    commit javascript and append

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,23 @@
-console.log('This is cool!');
+
+
+$(document).ready( function () {
+  var artwork, title, price, seller, everything;
+
+  elvis.results.forEach(function (items){
+     title = '<p class="etitle">' + items.title + '</p>';
+     artwork = '<img src= "'+ items.Images[0].url_170x135 +'" />';
+     price = '<p class="eprice">' + "$" + items.price + "USF" + '</p>';
+     seller = '<p class="eseller">' + items.Shop.shop_name + '</p>';
+     everything = '<div class="everything">' + artwork + title + seller + price + '</div>';
+
+         $('.maine').append(everything);
+
+  })
+
+
+
+
+});
+
+
+
